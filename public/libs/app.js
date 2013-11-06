72d9497e4ed1627110d988ae63d260e053ae1ce9
phyroxin <campslamin@blueyonder.co.uk>
2013-11-06 08:34:08 +0000

    third commit

--- public/libs/app.js
+++ public/libs/app.js
@@ -1,4 +1,115 @@
 (function($){
+	/*========================================================
+	 * Backbone Models test
+	 *========================================================
+	
+	Mate = Backbone.Model.extend({
+		defaults: {
+			'name': 'Darrell Bent'
+		}
+		,updateName: function(newName){this.set('name', newName);}
+	});
+	
+	firstMate = new Mate();
+	
+	console.log(firstMate.toJSON());
+	
+	firstMate.on('change:name', function(){
+		console.log('You\'ve updated the name');
+		console.log(this.get('name'));
+	});
+	 */
+	
+	//firstMate.updateName('Jim Broad');
+	
+	/*========================================================
+	 * Backbone Collections test
+	 *========================================================
+	 */
+	 
+	 Mate = Backbone.Model.extend();
+	 
+	 Mates = Backbone.Collection.extend({
+		 Model: Mate
+		,url: '#'
+	 });
+	 
+	 
+	 mates = new Mates([
+		 {name:'Bob Cantelli'}
+		,{name:'Susan Blisett'}
+		,{name:'Bob Monroe'}
+	 ]);
+	 
+	 mates.on('add remove', function(){
+		console.log('You\'ve changed the collection');
+	 });
+	 
+	 var firstMate = new Mate({
+		name:'Marco Polo'
+	 });
+	 
+	 mates.add(firstMate);
+	 
+	 mates.each(function(model){
+		console.log(model.get('name'));
+	 });
+	 
+	/*========================================================
+	 * Backbone Views test
+	 *========================================================
+	 */
+	 
+	 HomeView = Backbone.View.extend({
+		 
+		 el:'.backboneSandbox' 
+		 
+		,initialize: function(){
+			this.render();
+		 }
+		 
+		,render: function(){
+		
+			this.$el.empty();
+			
+			this.$el.append($('<h2 />', {
+								 'class':'h1Class'
+								,'style':'color:red;'
+								}).html('Backbone Sandbox')
+							);
+			
+			this.listView = new ListView();
+			this.$el.append(this.listView.render().el);
+			
+			return this;
+		}
+	 });
+
+	 ListView = Backbone.View.extend({
+		 tagName: 'ul'
+		
+		,initialize: function(){
+			this.template = _.template($('#item-container').html());
+		}
+
+		,render: function(){
+			var  el = this.$el
+				,template = this.template;
+			
+			el.empty();
+			
+			mates.each(function(mate){
+				el.append(template(mate.toJSON()));
+			});
+			
+			return this;
+		}
+	 });
+	 
+	 $(document).ready(function(){
+		mateApp = new HomeView();
+	 });
+	
 	
 	/*========================================================
 	 * Create a model to hold friend attribute
@@ -40,14 +151,16 @@
 			var friend_model = new Friend({name: friend_name});
 			var inputSuccess;
 			// Add a new friend model to our friend collection
-			$.get('/api/addfriend/'+friend_name, function(data){
-				inputSuccess = data.name;
-			});
+			if(friend_name){
+				$.get('/api/addfriend/'+friend_name, function(data){
+					inputSuccess = data.name;
+				});
+				if(inputSuccess!==""||inputSuccess!=='null')
+					this.friends.add(friend_model);
+				else
+					console.log('Something went wrong!');
+			}
 			
-			if(inputSuccess!==""||inputSuccess!=='null')
-				this.friends.add(friend_model);
-			else
-				console.log('Something went wrong!');
 		}
 		// This function will display the newly entered text and also display
 		// the results of the api call
@@ -70,4 +183,4 @@
 		}
 	});
 	var appView = new AppView;
-}($));
+}(jQuery));
